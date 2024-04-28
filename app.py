c68dfa24de824c00fbbd85aa5779b9d948649a71
ReinisVelmeris <103267351+ReinisVelmeris@users.noreply.github.com>
2024-04-28 23:19:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ def greetings():
     return jsonify({"greeting": "Hello from Python App!"})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port="3000")
+    # Default port is 7001 if no argument is provided
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 7001
+
+    # Run the Flask app on the specified port
+    app.run(host="0.0.0.0", debug=True, port=port)
